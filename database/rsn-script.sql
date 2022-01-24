24f992d8606bf671e10038cbc99a0471651c9976
Colin Knox <colin.r.knox@gmail.com>
2022-01-23 18:53:17 -0600

    final changes to sql script to make Hibernate easier

--- database/rsn-script.sql
+++ database/rsn-script.sql
@@ -7,57 +7,57 @@ DROP EXTENSION pgcrypto;
 CREATE EXTENSION pgcrypto;
 
 CREATE TABLE user_profiles (
-	up_id SERIAL,
-	up_email VARCHAR(254) NOT NULL UNIQUE,
-	up_first_name VARCHAR(50) NOT NULL,
-	up_last_name VARCHAR(50) NOT NULL,
-	up_pwd VARCHAR NOT NULL,
-	up_pwd_salt bytea NOT NULL,
-	up_about VARCHAR(500),
-	up_photo BYTEA,
-	up_creation_date TIMESTAMP,
-	PRIMARY KEY (up_id)
+	id SERIAL,
+	email VARCHAR(254) NOT NULL UNIQUE,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	"password" BYTEA NOT NULL,
+	password_salt BYTEA NOT NULL,
+	about VARCHAR(500),
+	photo BYTEA,
+	creation_date TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
 );
 
 
 CREATE TABLE user_posts (
-	post_id SERIAL,
-	post_up_id INT NOT NULL,
-	post_text VARCHAR(500),
-	post_image BYTEA,
-	PRIMARY KEY (post_id),
-	FOREIGN KEY (post_up_id) REFERENCES user_profiles (up_id)
+	id SERIAL,
+	author INT NOT NULL,
+	"text" VARCHAR(500) NOT NULL,
+	image BYTEA,
+	PRIMARY KEY (id),
+	FOREIGN KEY (author) REFERENCES user_profiles (id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE post_comments (
-	post_comment_id SERIAL,
-	post_comment_author INT NOT NULL,
-	post_comment_text VARCHAR(500) NOT NULL,
-	post_comment_post_id INT NOT NULL,
-	PRIMARY KEY (post_comment_id),
-	FOREIGN KEY (post_comment_author) REFERENCES user_profiles (up_id),
-	FOREIGN KEY (post_comment_post_id) REFERENCES user_posts (post_id) ON DELETE CASCADE
+	id SERIAL,
+	author INT NOT NULL,
+	"text" VARCHAR(500) NOT NULL,
+	post_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (author) REFERENCES user_profiles (id),
+	FOREIGN KEY (post_id) REFERENCES user_posts (id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE post_likes (
-	like_id SERIAL,
-	like_up_id INT NOT NULL,
-	like_p_id INT NOT NULL,
-	PRIMARY KEY (like_id),
-	FOREIGN KEY (like_up_id) REFERENCES user_profiles (up_id),
-	FOREIGN KEY (like_p_id) REFERENCES user_posts (post_id) ON DELETE CASCADE
+	id SERIAL,
+	profile_id INT NOT NULL,
+	post_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (profile_id) REFERENCES user_profiles (id),
+	FOREIGN KEY (post_id) REFERENCES user_posts (id) ON DELETE CASCADE
 );
 
 
-SELECT user_posts.post_id, post_likes.like_id, post_likes.like_up_id
+SELECT user_posts.id, post_likes.id, post_likes.profile_id
 FROM user_posts 
-RIGHT JOIN post_likes ON  user_posts.post_id =  post_likes.like_p_id;
+RIGHT JOIN post_likes ON  user_posts.id =  post_likes.post_id;
 
-SELECT user_posts.post_up_id, post_comments.post_comment_id, post_comments.post_comment_author, post_comments.post_comment_text
+SELECT user_posts.author, post_comments.id, post_comments.author, post_comments."text"
 FROM user_posts
-RIGHT JOIN post_comments ON  user_posts.post_id = post_comments.post_comment_post_id;
+RIGHT JOIN post_comments ON  user_posts.id = post_comments.post_id;
 
 
 --INSERT INTO user_profile VALUES (DEFAULT, 'user_profile_username',  crypt('user_profile_password', gen_salt('bf')), 'user_profile_name', 'user_profile_about', DEFAULT);
