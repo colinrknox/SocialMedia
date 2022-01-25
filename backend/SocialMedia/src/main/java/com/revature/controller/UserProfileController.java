e77ef0f3568e0a19a54c4deaba232856d5d0b7f0
Colin Knox <colin.r.knox@gmail.com>
2022-01-25 13:05:48 -0600

    small back-end changes

--- backend/SocialMedia/src/main/java/com/revature/controller/UserProfileController.java
+++ backend/SocialMedia/src/main/java/com/revature/controller/UserProfileController.java
@@ -1,7 +1,8 @@
 package com.revature.controller;
 
-import java.io.IOException;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.revature.model.UserProfile;
 import com.revature.service.UserProfileService;
 
@@ -21,14 +21,16 @@ public class UserProfileController {
 
 	@PostMapping(value = "/login")
 	@ResponseStatus(HttpStatus.ACCEPTED)
-	public UserProfile login(@RequestBody Map<String, Object> json) throws IOException {
+	public UserProfile login(HttpServletRequest req, @RequestBody Map<String, Object> json) {
 		UserProfile user = serv.authenticate((String)json.get("email"), (String)json.get("password"));
+		if (user != null)
+			req.getSession().setAttribute("account", user);
 		return user;
 	}
 
 	@PostMapping(value = "/register")
 	@ResponseStatus(HttpStatus.CREATED)
-	public UserProfile register(@RequestBody UserProfile user) throws JsonProcessingException {
+	public UserProfile register(@RequestBody UserProfile user) {
 		UserProfile userModified = serv.save(user);
 		return userModified;
 	}
