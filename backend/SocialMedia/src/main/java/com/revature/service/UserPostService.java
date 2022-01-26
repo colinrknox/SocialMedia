8a8c61741159e678d8e1e5917c3d9eff52046451
Colin Knox <colin.r.knox@gmail.com>
2022-01-26 09:36:13 -0600

    All user posts in descending date

--- backend/SocialMedia/src/main/java/com/revature/service/UserPostService.java
+++ backend/SocialMedia/src/main/java/com/revature/service/UserPostService.java
@@ -1,6 +1,12 @@
 package com.revature.service;
 
+import java.util.List;
+
+import com.revature.model.UserPost;
+
 public interface UserPostService {
 
 	int getPostLikes(int postId);
+	
+	List<UserPost> findAllPostsDesc();
 }
