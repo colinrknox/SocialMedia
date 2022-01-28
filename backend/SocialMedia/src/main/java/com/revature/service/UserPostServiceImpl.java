47b2cc96a765f53afef5a0028a301499460a18aa
Amir Mustafaa <amirmustafaa1@gmail.com>
2022-01-28 11:06:34 -0500

    Create Post and Create Like

--- backend/SocialMedia/src/main/java/com/revature/service/UserPostServiceImpl.java
+++ backend/SocialMedia/src/main/java/com/revature/service/UserPostServiceImpl.java
@@ -1,19 +1,30 @@
 package com.revature.service;
 
+import java.sql.Timestamp;
+
 import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.dao.PostLikeDao;
 import com.revature.dao.UserPostDao;
+import com.revature.model.PostLike;
 import com.revature.model.UserPost;
+
+
+
 
 @Service
 public class UserPostServiceImpl implements UserPostService {
 	
 	private PostLikeDao likeRepo;
 	private UserPostDao postRepo;
+	
+	public UserPostServiceImpl (final UserPostDao postRepo){
+	        this.postRepo = postRepo;
+	}
 	
 	@Override
 	public int getPostLikes(int postId) {
@@ -28,5 +39,20 @@ public class UserPostServiceImpl implements UserPostService {
 	@Autowired
 	public void setLikeRepo(PostLikeDao likeRepo) {
 		this.likeRepo = likeRepo;
+	}
+
+	@Override
+	public String createPost(int author, String text, String image, Timestamp creationDate) {
+		int id = 0;
+		UserPost post = new UserPost(id, author, text, image, creationDate);
+		postRepo.save(post);
+		return "Post Created";
+	}
+
+	@Override
+	public String createLike(int profileId, int postId) {
+		PostLike like = new PostLike(profileId, postId);
+		likeRepo.save(like);
+		return "Like Created";
 	}
 }
