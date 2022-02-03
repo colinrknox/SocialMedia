8dbf912a8ad84211689c5d5d4c388e8e0bfe0009
lyly2112 <lyly2112@gmail.com>
2022-02-02 19:30:10 -0800

    Add toggleMenu and updateAbout functionality.

---
+++ backend/SocialMedia/src/main/resources/static/assets/js/updateAbout.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    document.getElementById("about").onsubmit = updateAbout;
+}
+
+const submitUpdateAbout = async (event) => {
+    event.preventDefault();
+
+    
+}
+
+const updateAbout = async ()
