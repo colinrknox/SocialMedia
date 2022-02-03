ca8d72d5894b5b99091f0bc9c21b3cee85b85a84
unknown <lyly2112@gmail.com>
2022-02-03 01:04:16 -0800

    Fix updateAbout functionality.

--- backend/SocialMedia/src/main/resources/static/assets/js/updateAbout.js
+++ backend/SocialMedia/src/main/resources/static/assets/js/updateAbout.js
@@ -1,41 +1,87 @@
+const LOCAL_HOST = 'localhost:9001';
+
 window.onload = () => {
+    console.log("window.onload");
     document.getElementById("about").onsubmit = updateAbout;
 }
 
-const updateAbout = async (event) => {
+function updateAbout(event) {
+    console.log("updateAbout");
+
     event.preventDefault();
 
-    let about = document.getElementById("about_info_popup");
+    let about = document.getElementById("about_info_popup").value;
 
     submitUpdateAbout(about);
 }
 
-const submitUpdateAbout = async (about) => {
-    let postBody = JSON.stringify({
-        'about': about
-    })
+async function submitUpdateAbout (about) {
+    console.log("submitUpdateAbout");
+    console.log("about ===> ", about);
+
+    // let postBody = about;
 
     let result = await fetch(`http://${LOCAL_HOST}/about/save`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: postBody
+        body: about
     })
     .then(response  => {
         if (response.ok) {
             console.log("About update success");
             document.getElementById("about_info").innerText = about;
-            // loginFailAlert.classList.add('d-none');
-            // location.href = `http://${LOCAL_HOST}/feed.html`;
         } else {
             console.log("About update Failed #1");
-            // loginFailAlert.classList.remove('d-none');
         }
     })
     .catch(error => {
         console.log("About update Failed #2");
         console.log("error => ", error);
-        // loginFailAlert.classList.remove('d-none');
     });
 }
+
+// const updateAbout = async (event) => {
+//     console.log("updateAbout ");
+
+//     event.preventDefault();
+
+//     let about = document.getElementById("about_info_popup");
+
+//     submitUpdateAbout(about);
+// }
+
+// const submitUpdateAbout = async (about) => {
+
+//     console.log("submitUpdateAbout ");
+
+
+//     let postBody = JSON.stringify({
+//         'about': about
+//     })
+
+//     let result = await fetch(`http://${LOCAL_HOST}/about/save`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: postBody
+//     })
+//     .then(response  => {
+//         if (response.ok) {
+//             console.log("About update success");
+//             document.getElementById("about_info").innerText = about;
+//             // loginFailAlert.classList.add('d-none');
+//             // location.href = `http://${LOCAL_HOST}/feed.html`;
+//         } else {
+//             console.log("About update Failed #1");
+//             // loginFailAlert.classList.remove('d-none');
+//         }
+//     })
+//     .catch(error => {
+//         console.log("About update Failed #2");
+//         console.log("error => ", error);
+//         // loginFailAlert.classList.remove('d-none');
+//     });
+// }
