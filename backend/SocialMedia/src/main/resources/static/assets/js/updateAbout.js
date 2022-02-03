c5a607e0628c0d52c16ce1836eba006bdb6b0a31
lyly2112 <lyly2112@gmail.com>
2022-02-02 19:58:42 -0800

    Add updateInfo functionality.

--- backend/SocialMedia/src/main/resources/static/assets/js/updateAbout.js
+++ backend/SocialMedia/src/main/resources/static/assets/js/updateAbout.js
@@ -2,10 +2,40 @@ window.onload = () => {
     document.getElementById("about").onsubmit = updateAbout;
 }
 
-const submitUpdateAbout = async (event) => {
+const updateAbout = async (event) => {
     event.preventDefault();
 
-    
+    let about = document.getElementById("about_info_popup");
+
+    submitUpdateAbout(about);
 }
 
-const updateAbout = async ()
+const submitUpdateAbout = async (about) => {
+    let postBody = JSON.stringify({
+        'about': about
+    })
+
+    let result = await fetch(`http://${LOCAL_HOST}/about/save`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: postBody
+    })
+    .then(response  => {
+        if (response.ok) {
+            console.log("About update success");
+            document.getElementById("about_info").innerText = about;
+            // loginFailAlert.classList.add('d-none');
+            // location.href = `http://${LOCAL_HOST}/feed.html`;
+        } else {
+            console.log("About update Failed #1");
+            // loginFailAlert.classList.remove('d-none');
+        }
+    })
+    .catch(error => {
+        console.log("About update Failed #2");
+        console.log("error => ", error);
+        // loginFailAlert.classList.remove('d-none');
+    });
+}
